f4655c3161a8efa9e988f85748d2420e46339650
giuluck <giuluck9@gmail.com>
2025-07-20 02:30:15 +0200

    Refactor css file and fix other graphic issues

--- src/controller.js
+++ src/controller.js
@@ -32,20 +32,8 @@ angular.module("module", ["ngRoute", "ngSanitize"])
             }
         })
 
-        // set min-height of main section to have footer at the end every time an element or the entire window is resized
-        const main = $("main")[0]
-        const footer = $("footer")[0]
-        const observer = new ResizeObserver(_ => resize(main, footer))
-        observer.observe(main)
-        observer.observe(footer)
-        $(window).resize(() => resize(main, footer))
-
         // modify urls to include the full path to soundcloud while also setting them as trusted sources
         $scope.songs.forEach(song => {
             song.url = $sce.trustAsResourceUrl("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + song.url + "&color=%23579D6C&show_user=false")
         })
     })
-
-function resize(main, footer) {
-    main.style.minHeight = (window.innerHeight - footer.offsetHeight - 70) + "px"
-}
