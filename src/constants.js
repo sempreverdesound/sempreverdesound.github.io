e13f6db839c699a3913fa6b0979f32c3b69ea232
giuluck <giuluck9@gmail.com>
2026-03-05 10:52:57 +0100

    handle linting using prettier

--- src/constants.js
+++ src/constants.js
@@ -1,142 +1,143 @@
 const tracks = [
-    {
-        url: "2125855104",
-        alias: "campi-sterminati",
-        title: "Campi Sterminati",
-        credits: "Testo e Musica di Luca Giuliani"
-    },
-    {
-        url: "2194647963",
-        alias: "per-sempre",
-        title: "Per Sempre",
-        credits: "Testo e Musica di Luca Giuliani"
-    },
-    {
-        url: "2148266667",
-        alias: "cinderella-rave",
-        title: "Cinderella Rave",
-        credits: "Testo e Musica di Luca Giuliani"
-    },
-    {
-        url: "2108765244",
-        alias: "erbatoccare",
-        title: "Erbatoccare",
-        credits: "Testo e Musica di Luca Giuliani",
-    },
-    {
-        url: "2108764668",
-        alias: "le-mani-la-festa",
-        title: "Le Mani/La Festa",
-        credits: "Testo e Musica di Luca Giuliani",
-    },
-    {
-        url: "2108765919",
-        alias: "intro",
-        title: "Intro",
-        subtitle: "Empathy Is The Glue",
-        credits: "<div class='pb-1'>Testo campionato dal " +
-            "<a href='https://www.youtube.com/watch?v=Bx7z3sG7A9o' target='_blank'>discorso di Francesca Albanese</a>" +
-            " all'Assemblea Generale delle Nazioni Unite del 30 Ottobre 2024</div>" +
-            "<div>Musica di Luca Giuliani</div>"
-    },
-    {
-        url: "2194653291",
-        alias: "tutti-i-miei-sbagli",
-        title: "Tutti I Miei Sbagli",
-        credits: "Testo e Musica di Samuel Romano, Massimiliano Casacci, e Davide Dileo.",
-        subtitle: "Sempreverde Cover",
-        rework: "Cover"
-    },
-    {
-        url: "2194648227",
-        alias: "aurora-sogna",
-        title: "Aurora Sogna",
-        credits: "Testo e Musica di Samuel Romano, Massimiliano Casacci, e Davide Dileo.",
-        subtitle: "Sempreverde Cover",
-        rework: "Cover"
-    },
-    {
-        url: "2194653627",
-        alias: "rossa-palestina",
-        title: "Rossa Palestina",
-        credits: "Testo e Musica di Umberto Fiori.",
-        subtitle: "Sempreverde Remix",
-        rework: "Remix"
-    }
-]
+  {
+    url: "2125855104",
+    alias: "campi-sterminati",
+    title: "Campi Sterminati",
+    credits: "Testo e Musica di Luca Giuliani",
+  },
+  {
+    url: "2194647963",
+    alias: "per-sempre",
+    title: "Per Sempre",
+    credits: "Testo e Musica di Luca Giuliani",
+  },
+  {
+    url: "2148266667",
+    alias: "cinderella-rave",
+    title: "Cinderella Rave",
+    credits: "Testo e Musica di Luca Giuliani",
+  },
+  {
+    url: "2108765244",
+    alias: "erbatoccare",
+    title: "Erbatoccare",
+    credits: "Testo e Musica di Luca Giuliani",
+  },
+  {
+    url: "2108764668",
+    alias: "le-mani-la-festa",
+    title: "Le Mani/La Festa",
+    credits: "Testo e Musica di Luca Giuliani",
+  },
+  {
+    url: "2108765919",
+    alias: "intro",
+    title: "Intro",
+    subtitle: "Empathy Is The Glue",
+    credits:
+      "<div class='pb-1'>Testo campionato dal " +
+      "<a href='https://www.youtube.com/watch?v=Bx7z3sG7A9o' target='_blank'>discorso di Francesca Albanese</a>" +
+      " all'Assemblea Generale delle Nazioni Unite del 30 Ottobre 2024</div>" +
+      "<div>Musica di Luca Giuliani</div>",
+  },
+  {
+    url: "2194653291",
+    alias: "tutti-i-miei-sbagli",
+    title: "Tutti I Miei Sbagli",
+    credits: "Testo e Musica di Samuel Romano, Massimiliano Casacci, e Davide Dileo.",
+    subtitle: "Sempreverde Cover",
+    rework: "Cover",
+  },
+  {
+    url: "2194648227",
+    alias: "aurora-sogna",
+    title: "Aurora Sogna",
+    credits: "Testo e Musica di Samuel Romano, Massimiliano Casacci, e Davide Dileo.",
+    subtitle: "Sempreverde Cover",
+    rework: "Cover",
+  },
+  {
+    url: "2194653627",
+    alias: "rossa-palestina",
+    title: "Rossa Palestina",
+    credits: "Testo e Musica di Umberto Fiori.",
+    subtitle: "Sempreverde Remix",
+    rework: "Remix",
+  },
+];
 
 const links = [
-    {
-        href: "https://www.instagram.com/sempreverdesound",
-        sref: "instagram.com/sempreverdesound",
-        icon: "bi bi-instagram",
-        name: "Instagram",
-        type: "social",
-        split: "left"
-    },
-    {
-        href: "https://www.facebook.com/sempreverdesound",
-        sref: "facebook.com/sempreverdesound",
-        icon: "bi bi-facebook",
-        name: "Facebook",
-        type: "social",
-        split: "left"
-    },
-    {
-        href: "https://www.tiktok.com/@sempreverdesound",
-        sref: "tiktok.com/@sempreverdesound",
-        icon: "bi bi-tiktok",
-        name: "Tiktok",
-        type: "social",
-        split: "left"
-    },
-    {
-        href: "https://www.soundcloud.com/sempreverdesound",
-        sref: "soundcloud.com/sempreverdesound",
-        icon: "fa fa-soundcloud",
-        name: "SoundCloud",
-        type: "streaming",
-        split: "right"
-    },
-    {
-        href: "https://sempreverdesound.bandcamp.com",
-        sref: "sempreverdesound.bandcamp.com",
-        icon: "fa fa-bandcamp",
-        name: "BandCamp",
-        type: "streaming",
-        split: "right"
-    },
-    {
-        href: "https://drive.google.com/drive/folders/1F_qeK3JcW64sNFSjV3dBaHvmOcEEeueX",
-        sref: "drive.google.com/drive/folders/1F_qeK3JcW64sNFSjV3dBaHvmOcEEeueX",
-        icon: "bi bi-cloud-arrow-down-fill",
-        name: "Materiali",
-        type: "folder",
-        split: "left"
-    },
-    {
-        href: "mailto://luca.giuliani.11@gmail.com",
-        sref: "luca.giuliani.11@gmail.com",
-        icon: "bi bi-envelope-fill",
-        name: "Email",
-        type: "contact",
-        split: "right"
-    },
-    {
-        href: "tel://+393382037596",
-        sref: "(+39) 338 2037596",
-        icon: "bi bi-telephone-fill",
-        name: "Telefono",
-        type: "contact",
-        split: "right"
-    }
-]
+  {
+    href: "https://www.instagram.com/sempreverdesound",
+    sref: "instagram.com/sempreverdesound",
+    icon: "bi bi-instagram",
+    name: "Instagram",
+    type: "social",
+    split: "left",
+  },
+  {
+    href: "https://www.facebook.com/sempreverdesound",
+    sref: "facebook.com/sempreverdesound",
+    icon: "bi bi-facebook",
+    name: "Facebook",
+    type: "social",
+    split: "left",
+  },
+  {
+    href: "https://www.tiktok.com/@sempreverdesound",
+    sref: "tiktok.com/@sempreverdesound",
+    icon: "bi bi-tiktok",
+    name: "Tiktok",
+    type: "social",
+    split: "left",
+  },
+  {
+    href: "https://www.soundcloud.com/sempreverdesound",
+    sref: "soundcloud.com/sempreverdesound",
+    icon: "fa fa-soundcloud",
+    name: "SoundCloud",
+    type: "streaming",
+    split: "right",
+  },
+  {
+    href: "https://sempreverdesound.bandcamp.com",
+    sref: "sempreverdesound.bandcamp.com",
+    icon: "fa fa-bandcamp",
+    name: "BandCamp",
+    type: "streaming",
+    split: "right",
+  },
+  {
+    href: "https://drive.google.com/drive/folders/1F_qeK3JcW64sNFSjV3dBaHvmOcEEeueX",
+    sref: "drive.google.com/drive/folders/1F_qeK3JcW64sNFSjV3dBaHvmOcEEeueX",
+    icon: "bi bi-cloud-arrow-down-fill",
+    name: "Materiali",
+    type: "folder",
+    split: "left",
+  },
+  {
+    href: "mailto://luca.giuliani.11@gmail.com",
+    sref: "luca.giuliani.11@gmail.com",
+    icon: "bi bi-envelope-fill",
+    name: "Email",
+    type: "contact",
+    split: "right",
+  },
+  {
+    href: "tel://+393382037596",
+    sref: "(+39) 338 2037596",
+    icon: "bi bi-telephone-fill",
+    name: "Telefono",
+    type: "contact",
+    split: "right",
+  },
+];
 
 const photos = [
-    {name: "image1", alt: "Sempreverde - Foto 1"},
-    {name: "image2", alt: "Sempreverde - Foto 2"},
-    {name: "image3", alt: "Sempreverde - Foto 3"},
-    {name: "image4", alt: "Sempreverde - Foto 4"},
-    {name: "image5", alt: "Sempreverde - Foto 5"},
-    {name: "image6", alt: "Sempreverde - Foto 6"}
-]
+  { name: "image1", alt: "Sempreverde - Foto 1" },
+  { name: "image2", alt: "Sempreverde - Foto 2" },
+  { name: "image3", alt: "Sempreverde - Foto 3" },
+  { name: "image4", alt: "Sempreverde - Foto 4" },
+  { name: "image5", alt: "Sempreverde - Foto 5" },
+  { name: "image6", alt: "Sempreverde - Foto 6" },
+];
--- src/controller.js
+++ src/controller.js
@@ -1,40 +1,45 @@
-angular.module("module", ["ngRoute", "ngSanitize"])
+angular
+  .module("module", ["ngRoute", "ngSanitize"])
 
-    .config(function ($routeProvider, $locationProvider) {
-        $locationProvider.html5Mode(false)
-        $locationProvider.hashPrefix("")
-        $routeProvider
-            .when("/home", {templateUrl: "src/templates/home.html"})
-            .when("/contacts", {templateUrl: "src/templates/contacts.html"})
-            .when("/thanks", {templateUrl: "src/templates/thanks.html"})
-            .when("/presskit", {templateUrl: "src/templates/presskit.html"})
-            .when("/photos", {templateUrl: "src/templates/photos.html"})
-            .when("/rider", {templateUrl: "src/templates/rider.html"})
-            .otherwise({redirectTo: "/home"})
-        tracks.forEach(track => {
-            $routeProvider.when("/" + track.alias, {templateUrl: "src/templates/track.html"})
-        })
-    })
+  .config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(false);
+    $locationProvider.hashPrefix("");
+    $routeProvider
+      .when("/home", { templateUrl: "src/templates/home.html" })
+      .when("/contacts", { templateUrl: "src/templates/contacts.html" })
+      .when("/thanks", { templateUrl: "src/templates/thanks.html" })
+      .when("/presskit", { templateUrl: "src/templates/presskit.html" })
+      .when("/photos", { templateUrl: "src/templates/photos.html" })
+      .when("/rider", { templateUrl: "src/templates/rider.html" })
+      .otherwise({ redirectTo: "/home" });
+    tracks.forEach((track) => {
+      $routeProvider.when("/" + track.alias, { templateUrl: "src/templates/track.html" });
+    });
+  })
 
-    .controller("controller", function ($scope, $route, $sanitize, $sce) {
-        $scope.tracks = tracks
-        $scope.photos = photos
-        $scope.links = links
-        $scope.page = undefined
-        $scope.track = undefined
+  .controller("controller", function ($scope, $route, $sanitize, $sce) {
+    $scope.tracks = tracks;
+    $scope.photos = photos;
+    $scope.links = links;
+    $scope.page = undefined;
+    $scope.track = undefined;
 
-        // change page value and song index every time there is a new routing
-        // if a song page is selected, use generic tag "song" for any of the songs
-        $scope.$on('$routeChangeSuccess', function (event, route) {
-            $scope.page = route.$$route.originalPath.substring(1)
-            $scope.track = tracks.find(track => track.alias === $scope.page)
-            if ($scope.track !== undefined) {
-                $scope.page = ($scope.track.rework === undefined) ? "song" : "rework"
-            }
-        })
+    // change page value and song index every time there is a new routing
+    // if a song page is selected, use generic tag "song" for any of the songs
+    $scope.$on("$routeChangeSuccess", function (event, route) {
+      $scope.page = route.$$route.originalPath.substring(1);
+      $scope.track = tracks.find((track) => track.alias === $scope.page);
+      if ($scope.track !== undefined) {
+        $scope.page = $scope.track.rework === undefined ? "song" : "rework";
+      }
+    });
 
-        // modify urls to include the full path to soundcloud while also setting them as trusted sources
-        $scope.tracks.forEach(track => {
-            track.url = $sce.trustAsResourceUrl("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + track.url + "&color=%23579D6C&show_user=false")
-        })
-    })
+    // modify urls to include the full path to soundcloud while also setting them as trusted sources
+    $scope.tracks.forEach((track) => {
+      track.url = $sce.trustAsResourceUrl(
+        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" +
+          track.url +
+          "&color=%23579D6C&show_user=false",
+      );
+    });
+  });
